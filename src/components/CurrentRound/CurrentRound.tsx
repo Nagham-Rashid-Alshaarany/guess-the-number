40931f0ed8ff5857ca513feec492b48f0d64914b
Nagham Rashid Alshaarany <nagham.rashid.alshaarany@gmail.com>
2024-09-02 22:42:44 +0400

    create current round table

--- src/components/CurrentRound/CurrentRound.tsx
+++ src/components/CurrentRound/CurrentRound.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React from 'react';
+import { Table } from 'antd';
+import { useGameContext } from '../../context';
+import './CurrentRound.scss';
 
-export default function CurrentRound(){
-    return(
-        <></>
-    )
-}
+const CurrentRound: React.FC = () => {
+    const { round } = useGameContext();
+    const { players } = round;
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text: string, record: any) => (
+                <strong className={!round.isRunning ? (record.won ? 'won' : 'lost') : ''}>
+                    {text}
+                </strong>
+            ),
+        },
+        {
+            title: 'Point',
+            dataIndex: 'pointsPlaced',
+            key: 'pointsPlaced',
+            render: (text: number, record: any) => (
+                <span className={!round.isRunning ? (record.won ? 'won' : 'lost') : ''}>
+                    {text}
+                </span>
+            ),
+        },
+        {
+            title: 'Multiplier',
+            dataIndex: 'predictedMultiplier',
+            key: 'predictedMultiplier',
+            render: (text: number, record: any) => (
+                <span className={!round.isRunning ? (record.won ? 'won' : 'lost') : ''}>
+                    {text.toFixed(2)}
+                </span>
+            ),
+        },
+    ];
+
+    return (
+        <div className="current-round">
+            <div className='label'>
+                <img src={"/icons/cup_svgrepo.com.svg"} />
+                <label>Current Round</label>
+            </div>
+            <Table
+                columns={columns}
+                dataSource={players}
+                rowKey="name"
+                pagination={false}
+                bordered
+                className="current-round-table"
+            />
+        </div>
+    );
+};
+
+export default CurrentRound;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import PlayerInputs from "../../components/PlayerInputs";
 import GameBoard from "../../components/GameBoard";
 import SpeedSlider from "../../components/SpeedSlider";
+import CurrentRound from "../../components/CurrentRound";
 
 export default function Home() {
     return (
         <>
             <PlayerInputs />
+            <CurrentRound/>
             <SpeedSlider/>
             <GameBoard />
         </>
